851ec662e2efee3caa5750f23495936d18659afe
CooKingBeast <f62810@yahoo.com>
2021-10-21 16:20:32 +0500

    feat: add date and time with js

--- js/Clock.js
+++ js/Clock.js
@@ -1,9 +1,25 @@
-const time = document.querySelector('.time');
+const timeSelector = document.querySelector(".time");
+const dateSelector = document.querySelector(".date");
+
+function showDate() {
+  const date = new Date();
+  const options = {
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: "UTC",
+  };
+  const currentDate = date.toLocaleDateString("de-De", options);
+  dateSelector.textContent = currentDate;
+  console.log(currentDate, "curDate");
+}
 
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
-  time.textContent = currentTime;
+  timeSelector.textContent = currentTime;
+  setTimeout(showTime, 1000);
+  showDate();
 }
 showTime();
-
